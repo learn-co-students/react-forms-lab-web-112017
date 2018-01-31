0f08e66bab0e3564c68e39893544d6ac7817fc96
KellyStamps <kellydsample@gmail.com>
2018-01-31 18:50:01 -0500

    Done.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,25 +1,54 @@
 import React from "react";
 
 class LoginForm extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      password: ''
+    };
+  }
 
-    this.state = {};
+  handleFormChange = (e) => {
+    if (e.target.id === 'test-username') {
+      this.setState({
+        username: e.target.value
+      })
+    } else {
+      this.setState({
+        password: e.target.value
+      })
+    }
+  }
+
+  handleSubmit = () => {
+    this.props.onSubmit({username: this.state.username, password: this.state.password})
   }
 
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleSubmit}>
         <div>
           <label>
             Username
-            <input id="test-username" type="text" />
+            <input
+              id="test-username"
+              type="text"
+              name='username'
+              value={this.state.username}
+              onChange={this.handleFormChange}
+              />
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="test-password" type="password" />
+            <input
+              id="test-password"
+              type="password"
+              value={this.state.password}
+              onChange={this.handleFormChange}
+              />
           </label>
         </div>
         <div>
--- src/components/PoemWriter.js
+++ src/components/PoemWriter.js
@@ -4,15 +4,37 @@ class PoemWriter extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      content: ''
+    };
+  }
+
+  handleContentChange = (e) => {
+    this.setState({
+      content: e.target.value
+    })
+    this.handleError()
+  }
+
+  handleError = () => {
+    let poem = this.state.content.split('.');
+
+    if (poem[0].split(' ').length > 4 && poem.length > 3) {
+      document.getElementById('poem-validation-error').style.visibility = 'hidden'
+    }
+
+
+      else {
+      document.getElementById('poem-validation-error').style.visibility = 'visible'
+    }
   }
 
   render() {
     return (
       <div>
-        <textarea rows="3" cols="60" />
+        <textarea onChange={this.handleContentChange} rows="3" cols="60" />
         <div id="poem-validation-error" style={{ color: "red" }}>
-          This poem is not written in the right structure!
+          <p>This poem is not written in the right structure!</p>
         </div>
       </div>
     );
